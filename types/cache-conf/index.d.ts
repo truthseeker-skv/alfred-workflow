410694f592de3bb7bb2d930da35bf9ed16c4f2bd
truthseeker-skv <konstantin.sukovatij@gmail.com>
2021-04-23 20:15:28 +0300

    upd cache-conf typings

--- types/cache-conf/index.d.ts
+++ types/cache-conf/index.d.ts
@@ -1,11 +1,19 @@
 declare module 'cache-conf' {
   import Conf, { Options } from 'conf';
 
+  export interface SetDataOptions {
+    maxAge: number; // in ms
+  }
+
+  export interface GetDataOptions {
+    ignoreMaxAge?: boolean;
+  }
+
   class CacheConf extends Conf {
     constructor<T>(options: Options<T> | { version: string });
 
-    get<T>(key: string, options: any): T | undefined;
-    set<T>(key: string, val: T, options: any): void;
+    get<T>(key: string, options: GetDataOptions): T | undefined;
+    set<T>(key: string, val: T, options: SetDataOptions): void;
     has(key: string): boolean;
     isExpired(key: string): boolean;
   }
